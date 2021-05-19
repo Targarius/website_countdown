1d5974e58e885c4fa6cb919b5da15d4e0265b6a2
Targarius <theshark158@gmail.com>
2021-05-19 09:40:23 +0200

    js issue, don't reconize js file

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+let form = document.querySelector(".email_catcher");
+let input = document.querySelector(".email_input");
+
+const test = () =>{
+    console.log("testes");
+}
+
+function ValidateEmail(mail) 
+{
+    console.log(mail);
+//  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+//   {
+//     return (true)
+//   }
+//     alert("You have entered an invalid email address!")
+//     return (false)
+}
